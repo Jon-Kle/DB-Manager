98c76db6483c2e2ddb7d53e080ba0b0044f71c3e
_jokl_ <joni_kl@web.de>
2023-07-07 11:06:04 +0200

    changed setup.sql

--- setup/setup.sql
+++ setup/setup.sql
@@ -3,17 +3,20 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE `weatherdata` (
+CREATE DATABASE IF NOT EXISTS weather;
+USE weather;
+
+CREATE TABLE IF NOT EXISTS `data` (
   `entryDate` datetime DEFAULT NULL,
   `temp` float DEFAULT NULL,
   `pressure` float DEFAULT NULL,
   `hum` tinyint DEFAULT NULL,
   `windspeed` float DEFAULT NULL,
-  `winddir` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '---',
+  `winddir` varchar(3) CHARACTER SET utf8mb4 DEFAULT '---',
   `rainrate` float DEFAULT NULL,
   `uvindex` tinyint DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `weatherdata`
+ALTER TABLE `data`
   ADD UNIQUE KEY `entryDate` (`entryDate`);
 COMMIT;
